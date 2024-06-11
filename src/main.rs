947df4c14f619b4a3b46935b0ab298828e5fa592
joaoguibc <joaoguibc@gmail.com>
2024-06-10 23:14:52 -0300

    Create project

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+
+use std::io;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Hello, world!");
+}
